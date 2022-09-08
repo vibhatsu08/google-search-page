0fb2592ef02b77a4ed2c7a7630c088258cf45387
vibhatsu08 <mistryvedant17@gmail.com>
2022-09-08 14:07:10 +0530

    Added the reset search button in the project, now the search content can be cleared by clicking the 'x' button.

---
+++ script.js
@@ -0,0 +1,5 @@
+resetSearch = () => {
+    let resetSearchButton = document.querySelector("#reset-search-button");
+    let inputValue = document.querySelector("#search-bar");
+    inputValue.value = "";
+}
